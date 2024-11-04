f24b23593ef1bebcdace6bdaa5a6073c717eb8a3
Kaweme <67505756+mkaweme@users.noreply.github.com>
2024-11-04 23:23:29 +0200

    added about and hero components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import About from "./components/About";
+import Experience from "./components/Experience";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Experience />
+      <Services />
     </>
   );
 }
