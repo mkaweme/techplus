9afcf3396f76970fdead9e04b4de5c2200dfaafc
Kaweme <67505756+mkaweme@users.noreply.github.com>
2024-11-05 17:57:19 +0200

    Added footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Poppins } from "next/font/google";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
   title: "Techplus Industrial Solutions",
@@ -31,6 +32,7 @@ export default function RootLayout({
           <Navbar />
           {children}
         </main>
+        <Footer/>
       </body>
     </html>
   );
