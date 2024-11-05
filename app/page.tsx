09c39267b42df7f4d4d1c7ee61ab7526ed4977d3
Kaweme <67505756+mkaweme@users.noreply.github.com>
2024-11-05 17:57:36 +0200

    Added projects, Work and clients sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import About from "./components/About";
+import Clients from "./components/Clients";
 import Experience from "./components/Experience";
 import Hero from "./components/Hero";
+import Projects from "./components/Projects";
 import Services from "./components/Services";
+import Work from "./components/Work";
 
 export default function Home() {
   return (
@@ -10,6 +13,9 @@ export default function Home() {
       <About />
       <Experience />
       <Services />
+      <Projects />
+      <Clients />
+      <Work />
     </>
   );
 }
