ca435f2c82b841721a6cc348d439add2e91e0474
Kaweme <67505756+mkaweme@users.noreply.github.com>
2024-11-08 23:07:53 +0200

    nade component responsive

--- app/services/components/Services.tsx
+++ app/services/components/Services.tsx
@@ -20,38 +20,42 @@ const Services_Section = () => {
         </div>
       </div>
       <section className="flex flex-col w-full" id="construction">
-        <div className="flex w-full justify-center align-middle items-center bg-[url('/work_together_background.png')] bg-cover bg-fixed h-[300px]">
-          <h3 className="text-[36px] font-bold text-white text-center">CIVIL CONSTRUCTION WORKS AND MAINTENANCE SERVICES</h3>
+        <div className="flex w-full justify-center align-middle items-center bg-[url('/work_together_background.png')] bg-cover bg-fixed h-[200px] lg:h-[300px]">
+          <h3 className="text-[22px] lg:text-[36px] font-bold text-white text-center mx-1 lg:mx-0">
+            CIVIL CONSTRUCTION WORKS AND MAINTENANCE SERVICES
+          </h3>
         </div>
-        <div className="flex flex-col lg:flex-row mx-20 my-16">
-          <div className="w-[60%]">
-            <p className="text-[20px] mx-6">Techplus offers construction services from design to commissioning. We have an 
+        <div className="flex flex-col-reverse lg:flex-row mx-6 lg:mx-20 my-8 lg:my-16">
+          <div className="lg:w-[60%]">
+            <p className="text-[16px] lg:text-[20px] mx-1 lg:mx-6 my-6 lg:my-0">Techplus offers construction services from design to commissioning. We have an 
               expert team of civil engineers, technicians and bricklayers who can construct the following;
             </p>
-            <ul className="flex flex-col mx-12 gap-y-3 mt-4 text-[18px]">
+            <ul className="flex flex-col mx-1 lg:mx-12 gap-y-3 mt-4 text-[16px] lg:text-[18px]">
               <li className="flex flex-row"><Icon icon="octicon:dash-16" width="24" height="24" color="#F89E3E"/> Pavements</li>
               <li className="flex flex-row"><Icon icon="octicon:dash-16" width="24" height="24" color="#F89E3E"/> Residential, commercial and industrial infrastructure</li>
               <li className="flex flex-row"><Icon icon="octicon:dash-16" width="24" height="24" color="#F89E3E"/> Drainage systems</li>
               <li className="flex flex-row"><Icon icon="octicon:dash-16" width="24" height="24" color="#F89E3E"/> Road networks</li>
               <li className="flex flex-row"><Icon icon="octicon:dash-16" width="24" height="24" color="#F89E3E"/> Substation civil structure</li>
-              <li className="flex flex-row"><Icon icon="octicon:dash-16" width="24" height="24" color="#F89E3E"/> Renovation of Building/monuments/landmarks and Painting</li>
+              <li className="flex flex-row"><Icon icon="octicon:dash-16" width="24" height="24" color="#F89E3E"/> Renovation of Buildings/ monuments/ landmarks and Painting</li>
             </ul>
           </div>
           <div>
-            <Image src="/construction.png" alt="Construction" width={500} height={500} className="border-[14px] border-[#F89E3E]"/>
+            <Image src="/construction.png" alt="Construction" width={500} height={500} className="border-[6px] lg:border-[14px] border-[#F89E3E]"/>
           </div>
         </div>
       </section>
       <section className="flex flex-col w-full" id="electrical">
-        <div className="flex w-full justify-center align-middle items-center bg-[url('/work_together_background.png')] bg-cover bg-fixed h-[300px]">
-          <h3 className="text-[36px] font-bold text-white text-center">ELECTRICAL INSTALLATION AND MAINTENANCE SERVICES</h3>
+        <div className="flex w-full justify-center align-middle items-center bg-[url('/work_together_background.png')] bg-cover bg-fixed h-[200px] lg:h-[300px]">
+          <h3 className="text-[22px] lg:text-[36px] font-bold text-white text-center mx-1 lg:mx-0">
+            ELECTRICAL INSTALLATION AND MAINTENANCE SERVICES
+          </h3>
         </div>
-        <div className="flex flex-col lg:flex-row mx-20 my-16">
-          <div className="w-[60%]">
-            <p className="text-[20px] mx-6">Techplus can undertake a very wide scale of electrical projects ranging from 
+        <div className="flex flex-col-reverse lg:flex-row mx-6 lg:mx-20 my-8 lg:my-16">
+          <div className="lg:w-[60%]">
+            <p className="text-[16px] lg:text-[20px] mx-1 lg:mx-6 my-6 lg:my-0">Techplus can undertake a very wide scale of electrical projects ranging from 
               residential and domestic size projects and 240V to industrial and commercial projects of 66KV. This includes;
             </p>
-            <ul className="flex flex-col mx-12 gap-y-3 mt-4 text-[18px]">
+            <ul className="flex flex-col mx-1 lg:mx-12 gap-y-3 mt-4 text-[16px] lg:text-[18px]">
               <li className="flex flex-row"><Icon icon="octicon:dash-16" width="24" height="24" color="#F89E3E"/> 
                 Design and complete wiring of households, commercial and industrial infrastructure
               </li>
@@ -76,23 +80,23 @@ const Services_Section = () => {
             </ul>
           </div>
           <div>
-            <Image src="/electrical.png" alt="electrical_intallation" width={500} height={500} className="border-[14px] border-[#F89E3E]"/>
+            <Image src="/electrical.png" alt="electrical_intallation" width={500} height={500} className="border-[6px] lg:border-[14px] border-[#F89E3E]"/>
           </div>
         </div>
       </section>
       <section className="flex flex-col w-full" id="telecommunication">
-        <div className="flex w-full justify-center align-middle items-center bg-[url('/work_together_background.png')] bg-cover bg-fixed h-[300px]">
-          <h3 className="text-[36px] font-bold text-white text-center">
+        <div className="flex w-full justify-center align-middle items-center bg-[url('/work_together_background.png')] bg-cover bg-fixed h-[200px] lg:h-[300px]">
+          <h3 className="text-[22px] lg:text-[36px] font-bold text-white text-center mx-1 lg:mx-0">
             TELECOMMUNICATION INSTALLATION & MAINTENANCE SERVICES
           </h3>
         </div>
-        <div className="flex flex-col lg:flex-row mx-20 my-16">
-          <div className="w-[60%]">
-            <p className="text-[20px] mx-6">
+        <div className="flex flex-col-reverse lg:flex-row mx-6 lg:mx-20 my-8 lg:my-16">
+          <div className="lg:w-[60%]">
+            <p className="text-[16px] lg:text-[20px] mx-1 lg:mx-6 my-6 lg:my-0">
               We have an expert team of engineers, technicians and riggers which enables us to offer out the 
               following services in the telecommication sector;
             </p>
-            <ul className="flex flex-col mx-12 gap-y-3 mt-4 text-[18px]">
+            <ul className="flex flex-col mx-1 lg:mx-12 gap-y-3 mt-4 text-[16px] lg:text-[18px]">
               <li className="flex flex-row"><Icon icon="octicon:dash-16" width="24" height="24" color="#F89E3E"/>
                 Telecommunication tower installation and maintenance
               </li>
@@ -111,7 +115,7 @@ const Services_Section = () => {
             </ul>
           </div>
           <div>
-            <Image src="/telecommunication.png" alt="electrical_intallation" width={500} height={500} className="border-[14px] border-[#F89E3E]"/>
+            <Image src="/telecommunication.png" alt="electrical_intallation" width={500} height={500} className="border-[6px] lg:border-[14px] border-[#F89E3E]"/>
           </div>
         </div>
       </section>
