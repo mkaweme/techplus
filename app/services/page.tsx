a244d6674bef32b6ec3c0c3fc2dc2b6775090700
Kaweme <67505756+mkaweme@users.noreply.github.com>
2024-11-08 11:56:19 +0200

    changed to 2 component layout (services & projects)

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,36 +1,12 @@
 import React from "react";
-import { Icon } from "@iconify/react/dist/iconify.js";
+import Projects from "./components/Projects";
+import Services_Section from "./components/Services";
 
 const Services = () => {
   return (
     <>
-      <div className="flex flex-col items-center">
-        <div className="flex flex-row items-center my-6">
-          <div className="flex flex-col items-end gap-y-3 mr-1">
-            <div className="w-[65px] h-[2px] bg-[#F89E3E]"></div>
-            <div className="w-[40px] h-[2px] bg-[#F89E3E]"></div>
-          </div>
-          <h1 className="text-[#F89E3E] text-[24px] lg:text-[44px]">ALL  
-            <span className="text-[#000000] font-bold"> SERVICES</span>
-          </h1>
-          <div className="flex flex-col gap-y-3 ml-1">
-            <div className="w-[65px] h-[2px] bg-[#F89E3E]"></div>
-            <div className="w-[40px] h-[2px] bg-[#F89E3E]"></div>
-          </div>
-        </div>
-        <div className="flex flex-col">
-          <div className="border-l-[14px] border-[#F89E3E] pl-10">
-            <h3 className="text-[24px] font-bold">CIVIL CONSTRUCTION WORKS AND MAINTENANCE SERVICES</h3>
-            <p className="w-[80%]">Techplus offers construction services from design to commissioning. We have an 
-              expert team of civil engineers, technicians and bricklayers who can construct the 
-              following;
-            </p>
-          </div>
-          <ul>
-            <li>- Bricklaying</li>
-          </ul>
-        </div>
-      </div>
+      <Services_Section />
+      <Projects />
     </>
   );
 };
