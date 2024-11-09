816050bfb61f5b308e542eda52a06c02c6773482
Kaweme <67505756+mkaweme@users.noreply.github.com>
2024-11-09 14:33:41 +0200

    contct page, First commit

---
+++ app/components/Contact_Bar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+const Contact_Bar = () => {
+
+  return (
+    <>
+      <div className="hidden lg:flex flex-row bg-[#EE4036] w-full roboto">
+        <div className="flex flex-row w-[50%] ml-20 justify-around">
+          <div className="flex flex-row items-center">
+            <div className="flex flex-row p-1 m-2 bg-[#F89E3E]">
+              <Icon icon="uiw:mail" width="16" height="16" color="#FFFFFF"/>
+            </div>
+            <p className="text-white font-[500] text-[13px]">mushokenkandu3@gmail.com</p>
+          </div>
+          <div className="flex flex-row items-center">
+            <div className="flex flex-row p-1 m-2 bg-[#F89E3E]">
+              <Icon icon="mage:phone-fill" width="16" height="16" color="#FFFFFF"/>
+            </div>
+            <p className="text-white font-[500] text-[13px]">+260974963639</p>
+          </div>
+          <div className="flex flex-row items-center">
+            <div className="flex flex-row p-1 m-2 bg-[#F89E3E]">
+              <Icon icon="mdi:clock-outline" width="16" height="16" color="#FFFFFF" />
+            </div>
+            <p className="text-white font-[500] text-[13px]">Mon-Fri: 08:00 - 17:00Hrs</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact_Bar;
