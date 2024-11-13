0bcf3f9f1230465922de6db48180deb14ecadeb2
Kaweme <67505756+mkaweme@users.noreply.github.com>
2024-11-13 11:49:21 +0200

    updated address, added aria-labels to links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -12,23 +12,23 @@ const Footer = () => {
         </Link>
         <p className="text-white">We love to hear from you. You can reach out to us on our social media platforms</p>
         <div className="flex flex-row lg:mt-4 gap-x-4">
-          <a href="https://www.facebook.com" className="flex w-[40px] h-[40px] justify-center items-center border-[1px] border-[#00000045]">
+          <a href="https://www.facebook.com" aria-label="link to facebook" className="flex w-[40px] h-[40px] justify-center items-center border-[1px] border-[#00000045]">
             <Icon icon="ri:facebook-fill" width="30" height="30" color="#F89E3E" />
           </a>
-          <a href="https://www.x.com" className="flex w-[40px] h-[40px] justify-center items-center border-[1px] border-[#00000045]">
+          <a href="https://www.x.com" aria-label="link to twitter" className="flex w-[40px] h-[40px] justify-center items-center border-[1px] border-[#00000045]">
             <Icon icon="pajamas:twitter" width="30" height="30" color="#F89E3E" />
           </a>
-          <a href="https://www.linkedin.com" className="flex w-[40px] h-[40px] justify-center items-center border-[1px] border-[#00000045]">
+          <a href="https://www.linkedin.com" aria-label="link to linkedin" className="flex w-[40px] h-[40px] justify-center items-center border-[1px] border-[#00000045]">
             <Icon icon="ri:linkedin-fill" width="30" height="30" color="#F89E3E" />
           </a>
-          <a href="https://www.instagram.com" className="flex w-[40px] h-[40px] justify-center items-center border-[1px] border-[#00000045]">
+          <a href="https://www.instagram.com" aria-label="link to instagram" className="flex w-[40px] h-[40px] justify-center items-center border-[1px] border-[#00000045]">
             <Icon icon="mdi:instagram" width="30" height="30" color="#F89E3E" />
           </a>
         </div>
       </div>
       <div className="flex flex-col lg:w-[25%] text-white gap-y-3">
         <h3 className="text-[22px] font-bold my-6">CONTACT US</h3>
-        <p>Plot 1080, Off Nationalsit Road, Lusaka</p>
+        <p>Plot 002, Off Mungwi Road, Chikondano Lusaka West, Lusaka</p>
         <p>mushokenkandu3@gmail.com</p>
         <p>josephyoka@gmail.com</p>
         <p>(+260)-976-055210</p>
