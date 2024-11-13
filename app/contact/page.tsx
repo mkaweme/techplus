180510358771e906b31b331c31697d8f156e7cd1
Kaweme <67505756+mkaweme@users.noreply.github.com>
2024-11-13 11:51:39 +0200

    updated email address

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -21,14 +21,15 @@ const Contact = () => {
          <h1>LOCATION</h1>
       </div>
       <div className="flex flex-col lg:flex-row lg:gap-x-5 w-full mt-14 mx-20 justify-center text-[14px] text-[#626262]">
-        <div className="relative flex flex-col lg:w-[15%] items-center pt-16 border-[1px] p-3 my-9 mx-12 lg:m-3 h-40">
+        <div className="relative flex flex-col lg:w-[15%] items-center pt-16 border-[1px] p-3 my-9 mx-12 lg:m-3 h-48">
           <Icon icon="mdi:location" width="100" height="100" color="#F89E3E" className="absolute border-[1px] p-6 rounded-full -top-12 bg-white"/>
           <h6 className="my-1 text-black font-semibold">Office location</h6>
-          <p>Plot 1080</p>
-          <p>Off Nationalsit Road</p>
+          <p>Plot 002</p>
+          <p>Off Mungwi Road</p>
+          <p>Chikondano Lusaka West</p>
           <p>Lusaka</p>
         </div>
-        <div className="relative flex flex-col lg:w-[15%] items-center pt-16 border-[1px] p-3 my-9 mx-12 lg:m-3 h-40">
+        <div className="relative flex flex-col lg:w-[15%] items-center pt-16 border-[1px] p-3 my-9 mx-12 lg:m-3 h-48">
           <div className="absolute border-[1px] p-6 rounded-full -top-12 bg-white">
             <Icon icon="mdi:clock" width="50" height="50" color="#F89E3E"/>
           </div>
@@ -37,7 +38,7 @@ const Contact = () => {
           <p>Sat: 08:00 - 12:30Hrs</p>
           <p>Sun: Closed</p>
         </div>
-        <div className="relative flex flex-col lg:w-[15%] items-center pt-16 border-[1px] p-3 my-9 mx-12 lg:m-3 h-40">
+        <div className="relative flex flex-col lg:w-[15%] items-center pt-16 border-[1px] p-3 my-9 mx-12 lg:m-3 h-48">
           <div className="absolute border-[1px] p-6 rounded-full -top-12 bg-white">
             <Icon icon="ic:baseline-phone" width="50" height="50" color="#F89E3E"/>
           </div>
@@ -45,7 +46,7 @@ const Contact = () => {
           <p>+260 974963639</p>
           <p>+260 976055210</p>
         </div>
-        <div className="relative flex flex-col lg:w-[15%] items-center pt-16 border-[1px] p-3  my-9 mx-12 lg:m-3 h-40">
+        <div className="relative flex flex-col lg:w-[15%] items-center pt-16 border-[1px] p-3  my-9 mx-12 lg:m-3 h-48">
           <div className="absolute border-[1px] p-6 rounded-full -top-12 bg-white">
             <Icon icon="uiw:mail" width="50" height="50" color="#F89E3E"/>
           </div>
