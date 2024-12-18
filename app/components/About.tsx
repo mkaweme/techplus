cc02d37a2ba30605d0a049cc3955e32e0de02139
Kaweme <67505756+mkaweme@users.noreply.github.com>
2024-12-18 17:04:42 +0200

    added fullstops

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -23,11 +23,11 @@ const About = () => {
           <div className="absolute bg-white lg:w-[40%] p-4 mx-6 lg:mx-0 h-72 lg:h-96 top-52 lg:top-32 lg:-right-48 z-10">
             <h1 className="text-[27px] font-bold">Commitment to excellence</h1>
             <p className="text-[14px] font-medium my-5">We are a team with integrity, professionalism and abide
-              by the ethical codes of project management
+              by the ethical codes of project management.
             </p>
             <p className="text-[14px]">
               Customer satisfaction is our utmost priority coupled with efficient use
-              of materials to meet our business goals
+              of materials to meet our business goals.
             </p>
           </div>
         </div>
