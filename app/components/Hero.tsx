6c6ed15d8547cdc06cc2aef95fed880c253f6d1b
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-01-07 13:51:00 +0200

    Changed to new wording

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,10 +7,13 @@ const Hero = () => {
     <>
       <div className="lg:h-[85vh]">
         <div className="flex flex-col lg:flex-row">
-          <div className="flex flex-col relative lg:w-[50%] ml-10 lg:mx-20 my-10 lg:my-20 h-96 justify-center">
-            <h1 className="text-[34px] lg:text-[60px] font-black lg:font-bold ml-10 lg:ml-16 z-10">WE BUILD YOUR DREAM</h1>
-            <p className="ml-10 lg:ml-16 z-10">We are your number 1 engineering contractor and 
-              industrial supplier.
+          <div className="flex flex-col relative lg:w-[50%] ml-10 lg:mx-20 my-10 lg:my-20 h-96 justify-center gap-y-8">
+            <h1 className="text-[34px] lg:text-[52px] font-black lg:font-bold ml-10 lg:ml-16 z-10">WE BUILD YOUR DREAM</h1>
+            <p className="text-[13px] ml-10 lg:ml-16 z-10">As a premier engineering contracting and industrial supply firm, we specialize
+              in delivering comprehensive solutions that cater to the diverse needs of our clients. With a strong focus on 
+              technical expertise, innovative problem-solving, and exceptional customer service, we have 
+              established ourselves as a trusted partner for industries seeking reliable and efficient engineering and supply
+              chain solutions.
             </p>
             <div className="absolute top-0 border-[20px] border-[#F89E3E] w-48 lg:w-72 h-96 z-0"></div>
           </div>
