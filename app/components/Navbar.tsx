f3b342d1c878b7866b5b8d14c124994095476986
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-01-22 21:30:01 +0200

    added mobile nav styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -12,24 +12,22 @@ type navlink = {
 
 const NAVLINKS: navlink[] = [
   { title: "HOME", link: "/" },
-  { title: "SERVICES", link: "/services" },
-  { title: "ABOUT", link: "/about" },
-  { title: "CONTACT US", link: "/contact" },
+  { title: "SERVICES", link: "/services/" },
+  { title: "ABOUT", link: "/about/" },
+  { title: "CONTACT US", link: "/contact/" },
 ];
 const Navbar = () => {
 
   //Create state variables
   const [mNavOpen, setMNavOpen] = useState<boolean>(false);
-
-
   const pathName: string = usePathname();
 
   return (
       <nav className="flex flex-col w-full z-20">
         <div className="flex flex-row w-full justify-between items-center lg:items-end bg-[#350F2B] z-10">
           <Link href="/" className="ml-8 lg:ml-20">
-            <Image src="/logo.png" alt="logo" width={80} height={80} className="hidden lg:block"/>
-            <Image src="/logo_mini.png" alt="logo" width={80} height={80} onClick={() => setMNavOpen(false)} className="block lg:hidden mr-10"/>
+            <Image src="/logo.png" alt="logo" width={496} height={496} className="hidden lg:block lg:w-20 lg:h-auto"/>
+            <Image src="/logo_mini.png" alt="logo" width={378} height={320} onClick={() => setMNavOpen(false)} className="block lg:hidden mr-10 w-20 h-auto"/>
           </Link>
           {
             mNavOpen ? (
@@ -40,9 +38,8 @@ const Navbar = () => {
           }
           <div className="hidden lg:flex gap-x-16 mr-20 h-full">
             {NAVLINKS.map((link, index) => {
-              const isActive = pathName.endsWith(link.link);
               return (
-                <Link key={index} href={link.link} className={isActive ? "text-[#F89E3E] font-bold text-[14px] border-b-[3px] border-[#F89E3E] pb-3 px-1": "font-semibold text-[14px] text-[#D9D2D6] hover:text-[#F89E3E] pb-3 px-1"}>
+                <Link key={index} href={link.link} className={ (pathName === link.link) ? "text-[#F89E3E] font-bold text-[14px] border-b-[3px] border-[#F89E3E] pb-3 px-1": "font-semibold text-[14px] text-[#D9D2D6] hover:text-[#F89E3E] pb-3 px-1"}>
                   {link.title}
                 </Link>
               );
@@ -51,7 +48,7 @@ const Navbar = () => {
         </div>
         { mNavOpen && (
           <div onClick={() => setMNavOpen(false)} className="bg-white">
-            <MobileNavbar/>
+            <MobileNavbar pathName={pathName}/>
           </div>
         )}
       </nav>
@@ -60,12 +57,17 @@ const Navbar = () => {
 
 export default Navbar;
 
-const MobileNavbar = () => {
+type MobileNavbarProps = {
+  pathName: string
+}
+
+const MobileNavbar: React.FC<MobileNavbarProps> = ({ pathName }) => {
   return (
     <div className="flex flex-col gap-y-4 mt-4 mx-4">
       {NAVLINKS.map((link, index) => {
+        const isActive: boolean = pathName === link.link;
         return (
-          <Link key={index} href={link.link} className={"hover:text-[#F89E3E] font-semibold text-[14px] border-b-2"}>
+          <Link key={index} href={link.link} className={ isActive ? "font-semibold text-[#F89E3E] border-b-2" :"hover:text-[#F89E3E] font-semibold text-[14px] border-b-2"}>
             {link.title}
           </Link>
         );
