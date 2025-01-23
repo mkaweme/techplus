e4974097c1a702f055bb497959f9b438c92f4ca0
Kaweme <67505756+mkaweme@users.noreply.github.com>
2025-01-23 17:11:14 +0200

    updated image layout and dimensions

--- app/components/Clients.tsx
+++ app/components/Clients.tsx
@@ -21,14 +21,14 @@ const Clients = () => {
         <p className="text-[18px] mx-2 lg:mx-0 text-centermb-2">These are some of our esteemed clients.We take pride in the strong 
           relationships we&apos;ve built with these exceptional companies.
         </p>
-        <div className="grid grid-cols-3 lg:grid-cols-7 gap-10 lg:gap-20 mx-3 lg-0">
-          <Image src={"/dr_javias.png"} alt="Dr Javias" width={100} height={100} />
-          <Image src={"/chisambatc.png"} alt="Chisamba Town Council" width={100} height={100} />
-          <Image src={"/mylan_2.png"} alt="Mylan" width={100} height={100} />
-          <Image src={"/chavumatc.png"} alt="Chavuma Town Council" width={100} height={100} />
-          <Image src={"/national_assemblies.png"} alt="National Assembly" width={100} height={100} />
-          <Image src={"/chirundutc.png"} alt="Chirundu Town Coucil" width={100} height={100} />
-          <Image src={"/incon.png"} alt="Incon Enterprises" width={100} height={100} />
+        <div className="grid grid-cols-3 lg:grid-cols-7 gap-10 lg:gap-20 mx-3 lg-0 items-center">
+          <Image src={"/dr_javias.png"} alt="Dr Javias" width={486} height={481} className="w-[100px] h-auto"/>
+          <Image src={"/chisambatc.png"} alt="Chisamba Town Council" width={292} height={250} className="w-[100px] h-auto"/>
+          <Image src={"/mylan_2.png"} alt="Mylan" width={459} height={161} className="w-[100px] h-auto"/>
+          <Image src={"/chavumatc.png"} alt="Chavuma Town Council" width={513} height={435} className="w-[100px] h-auto"/>
+          <Image src={"/national_assemblies.png"} alt="National Assembly" width={377} height={373} className="w-[100px] h-auto"/>
+          <Image src={"/chirundutc.png"} alt="Chirundu Town Coucil" width={255} height={324} className="w-[100px] h-auto" />
+          <Image src={"/incon.png"} alt="Incon Enterprises" width={481} height={192} className="w-[100px] h-auto"/>
         </div>
       </div>
     </>
